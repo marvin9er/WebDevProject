ffa3316637660ceb19b31118be133ba1d497b765
Justin Horton <justinhorton125@gmail.com>
2019-10-23 13:11:43 -0500

    Adding Site

---
+++ about.php
@@ -0,0 +1,48 @@
+<?php 
+	$podcast_names = [
+		'BJ²', 
+		'Autism Lite', 
+		'The Random Shit Burrito', 
+		'Grand Stimulation Center', 
+		'3 dudes 1 mic', 
+		'Whole Lotta weird'
+	];
+
+
+	$name_number = rand(0, count($podcast_names)-1);
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title><?=$podcast_names[$name_number]?></title>
+</head>
+<body>
+	<div id="header">
+		<h1><?=$podcast_names[$name_number]?></h1>
+	</div>
+	<div id="menubar">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="episodes.php">Episodes</a></li>
+			<li><a href="faq.php">FAQ</a></li>
+		</ul>
+	</div>
+	<div id="content">
+		<h2>About</h2>
+		<?php
+			$myfile = fopen("about.txt", "r") or die("Unable to open file!");
+			echo fread($myfile,filesize("about.txt"));
+			fclose($myfile);
+		?> 
+	</div>
+	<div id="footer">
+		
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php 
+	$podcast_names = [
+		'BJ²', 
+		'Autism Lite', 
+		'The Random Shit Burrito', 
+		'Grand Stimulation Center', 
+		'3 dudes 1 mic', 
+		'Whole Lotta weird'
+	];
+
+
+	$name_number = rand(0, count($podcast_names)-1);
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title><?=$podcast_names[$name_number]?></title>
+</head>
+<body>
+	<div id="header">
+		<h1><?=$podcast_names[$name_number]?></h1>
+	</div>
+	<div id="menubar">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="episodes.php">Episodes</a></li>
+			<li><a href="faq.php">FAQ</a></li>
+		</ul>
+	</div>
+	<div id="content">
+		<?php
+			$myfile = fopen("index.txt", "r") or die("Unable to open file!");
+			echo fread($myfile,filesize("index.txt"));
+			fclose($myfile);
+		?> 
+	</div>
+	<div id="footer">
+		
+	</div>
+</body>
+</html>
