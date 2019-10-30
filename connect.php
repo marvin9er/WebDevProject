7efbd0eedfe294b7728349d89e248346dacbcf43
Justin Horton <justinhorton125@gmail.com>
2019-10-30 13:15:11 -0500

    Created Connect.php
    Made the connection to the database

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+    define('DB_DSN','mysql:host=localhost;dbname=id11413473_bjsquared');
+    define('DB_USER','id11413473_bjadmin');
+    define('DB_PASS','ZIBA6fFyjDTn&3uiCk2#');     
+    
+    try {
+        $db = new PDO(DB_DSN, DB_USER, DB_PASS);
+    } catch (PDOException $e) {
+        print "Error: " . $e->getMessage();
+        die(); // Force execution to stop on errors.
+    }
+?>
