ef1616fae8c5b150dedbae950de4565ebabf4c03
Justin Horton <justinhorton125@gmail.com>
2019-11-19 14:39:59 -0600

    Update index.php
    Creates a cookie on successfull login, as well as updates a post if user edited it.
    Added ability to access the log in and sign out page

--- index.php
+++ index.php
@@ -1,5 +1,45 @@
 <?php
 	include 'connect.php';
+
+	if(isset($_POST['username']) && isset($_POST['password']))
+	{
+		$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_EMAIL);
+
+		$stmt = $db->prepare("SELECT * FROM users WHERE username LIKE '$username' AND password LIKE '$password'");
+        $stmt->execute();
+
+        if ($stmt->rowCount() == 1) {
+        	ini_set('session.gc_maxlifetime', 60);
+
+        	session_start();
+
+        	$row = $stmt->fetch();
+
+        	$_SESSION['username'] = $username;
+        	$_SESSION['isAdmin'] = $row['isAdmin'];
+
+        	$cookie_name = "User";
+			$cookie_value = $row['userID'];
+			setcookie($cookie_name, $cookie_value, time() + (3600), "/");
+        }
+	}
+
+	if (isset($_POST['contentArea']) && filter_input(INPUT_GET, 'postID', FILTER_VALIDATE_INT)) 
+	{
+
+		$postID = filter_input(INPUT_GET, "postID", FILTER_SANITIZE_NUMBER_INT);
+		$contentDescription = filter_input(INPUT_POST, "contentArea", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		
+   		$statement = $db->prepare("UPDATE posts SET contentDescription=:description WHERE postID=:id");
+   		$statement->bindParam(':description', $contentDescription);
+    	$statement->bindParam(':id', $postID);
+    	$statement->execute();
+	}
+
+
+
+
 ?>
 
 
@@ -9,7 +49,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Whole Lotta weird</title>
+	<title>Awakening</title>
 	<link rel="icon" type="image/png" href="Utilities/icon.png" />
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Finger+Paint&display=swap" rel="stylesheet"> 
@@ -26,8 +66,13 @@
 				<li class="breadcrumb-item active" aria-current="page">Home</li>
 				<li class="breadcrumb-item"><a href="about.php">About</a></li>
 				<li class="breadcrumb-item"><a href="episodes.php">Episodes</a></li>
-				<li class="breadcrumb-item"><a href="News.php">News</a></li>
+				<li class="breadcrumb-item"><a href="news.php">News</a></li>
 				<li class="breadcrumb-item"><a href="faq.php">FAQ</a></li>
+				<?php if (isset($_COOKIE["User"])):?>
+					<li class="breadcrumb-item"><a href="signout.php">Sign Out</a></li>
+				<?php else: ?>
+					<li class="breadcrumb-item"><a href="login.php">Login</a></li>
+				<?php endif ?>
 			</ol>
 
 		</nav>
